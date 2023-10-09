20989f9ac7731dfae0b83af0edeec73ffa2a761c
jdun0012 <64505454+jdun0012@users.noreply.github.com>
2023-10-09 22:54:41 +1100

    Add files via upload

---
+++ COBar.js
@@ -0,0 +1,4 @@
+var vg_6 = "CO_Bar_Chart.json";
+vegaEmbed("#COBar", vg_6).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

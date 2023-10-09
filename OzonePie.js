2bb556352ff38a0ec8d5dd18aed716116d87a02b
jdun0012 <64505454+jdun0012@users.noreply.github.com>
2023-10-09 22:52:39 +1100

    Add files via upload

---
+++ OzonePie.js
@@ -0,0 +1,4 @@
+var vg_7 = "OzonePie.json";
+vegaEmbed("#OzPie", vg_7).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
